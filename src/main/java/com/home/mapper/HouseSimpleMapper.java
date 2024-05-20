d3673a680c7e7f86707441cf27f88be077a652da
Yangseyeon <seyeon0207@gmail.com>
2024-05-20 19:56:59 +0900

    feat: search saleList, complexSimpleList by dongCode

--- src/main/java/com/home/mapper/HouseSimpleMapper.java
+++ src/main/java/com/home/mapper/HouseSimpleMapper.java
@@ -10,4 +10,5 @@ import com.home.dto.HouseSimpleDto;
 @Mapper
 public interface HouseSimpleMapper {
 	List<HouseSimpleDto> findAptListByLngLatRange(@Param("sLat") String sLat, @Param("eLat") String eLat, @Param("sLng") String sLng, @Param("eLng") String eLng);
+	List<HouseSimpleDto> findAptListByDongCode(@Param("dongCode") String dongCode);
 }
--- src/main/java/com/home/service/HouseSimpleService.java
+++ src/main/java/com/home/service/HouseSimpleService.java
@@ -2,12 +2,14 @@ package com.home.service;
 
 import java.util.List;
 
+
 import com.home.dto.DongCodeDto;
 import com.home.dto.HouseSimpleDto;
 
 public interface HouseSimpleService {
 	List<HouseSimpleDto> findAptListByLngLatRange(String sLat, String eLat, String sLng, String eLng);
+	
 	List<DongCodeDto> findAptListGroupByDongByLngLatRange(String sLat, String eLat, String sLng, String eLng);
 	
-
+	List<HouseSimpleDto> findAptListByDongCode(String dongCode);
 }
